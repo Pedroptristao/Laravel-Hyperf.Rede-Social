b9e6b867c760d8a1979bdfcf57e5d084a94da98c
pedroptristao <pedroptristao@hotmail.com>
2024-03-09 09:45:55 -0300

    #setup - adicionada api laravel ao projeto

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hyperf
 Neste projeto terei meu primeiro contato com Hyperf e Swoole
+sudo docker-compose run --rm --service-ports hyperf-skeleton
+php bin/hyperf.php server:watch
---
+++ hyperf-app/app/Model/User.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace App\Model;
+
+use Hyperf\DbConnection\Model\Model;
+
+/**
+ * @property $id
+ * @property $name
+ * @property $email
+ * @property $password
+ * @property $created_at
+ * @property $updated_at
+ */
+class User extends Model
+{
+    /**
+     * @var string
+     */
+    public $keyType = 'string';
+
+    /**
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['id', 'name', 'email', 'password', 'created_at', 'updated_at'];
+}
