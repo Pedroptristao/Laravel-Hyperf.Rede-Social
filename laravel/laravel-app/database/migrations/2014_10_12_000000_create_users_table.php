18b2de4184bfc289619b036e1b0cea369b141e98
pedroptristao <pedroptristao@hotmail.com>
2024-03-09 17:23:05 -0300

    [feat][user] - modelagem de usuarios

--- laravel/laravel-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/laravel-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,25 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_photo_path')->nullable();
+            $table->enum('theme', ['light', 'dark'])->default('light');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('users');
