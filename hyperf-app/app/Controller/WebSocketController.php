5884a8a93d3cc9469e0664c4a3ac5326477669c9
pedroptristao <pedroptristao@hotmail.com>
2024-11-18 16:09:56 -0300

    test sign

--- hyperf-app/app/Controller/WebSocketController.php
+++ hyperf-app/app/Controller/WebSocketController.php
@@ -6,13 +6,19 @@ namespace App\Controller;
 use Hyperf\Contract\OnCloseInterface;
 use Hyperf\Contract\OnMessageInterface;
 use Hyperf\Contract\OnOpenInterface;
+use Swoole\WebSocket\Frame;
+use Swoole\WebSocket\Server as WebSocketServer;
+use Swoole\Http\Request;
 
 class WebSocketController implements OnMessageInterface, OnOpenInterface, OnCloseInterface
 {
-    private $clients = [];
+    private array $clients = [];
 
     public function onMessage($server, $frame): void
     {
+        $message = json_decode($frame->data, true);
+        $message['sender'] = $this->clients[$frame->fd];
+
         foreach ($this->clients as $fd => $client) {
             $server->push($fd, json_encode(['type' => 'message', 'sender' => $client, 'message' => $frame->data]));
         }
@@ -26,14 +32,11 @@ class WebSocketController implements OnMessageInterface, OnOpenInterface, OnClos
 
     public function onOpen($server, $request): void
     {
-        if (!isset($this->clients[$request->fd])) {
-            $this->clients[$request->fd] = $request->fd;
-            $this->broadcastConnectedUsers($server);
-        }
+        $this->clients[$request->fd] = 'User ' . $request->fd;
+        $this->broadcastConnectedUsers($server);
     }
-    
 
-    private function broadcastConnectedUsers($server)
+    private function broadcastConnectedUsers(WebSocketServer $server): void
     {
         $connectedUsers = count($this->clients);
         foreach ($this->clients as $fd => $client) {
@@ -41,3 +44,4 @@ class WebSocketController implements OnMessageInterface, OnOpenInterface, OnClos
         }
     }
 }
+
